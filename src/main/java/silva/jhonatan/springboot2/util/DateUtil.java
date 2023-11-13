3280d5437da741750c4921fbbffbf7072d209deb
jsilva6 <jhonatan.silva@fazenda.rj.gov.br>
2023-11-13 11:55:51 -0300

    Aula 06

--- src/main/java/silva/jhonatan/springboot2/util/DateUtil.java
+++ src/main/java/silva/jhonatan/springboot2/util/DateUtil.java
@@ -2,12 +2,11 @@ package silva.jhonatan.springboot2.util;
 
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 /**
  * Component é uma anotação que transforma a classe em spring bean
  */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 @Component
 public class DateUtil {
     public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){

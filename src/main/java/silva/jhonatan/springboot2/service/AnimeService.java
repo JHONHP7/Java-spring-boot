308f6981f25904f703b92bb99219a00d5113f194
jsilva6 <jhonatan.silva@fazenda.rj.gov.br>
2023-11-14 12:07:12 -0300

    Aula 09 - Método POST

--- src/main/java/silva/jhonatan/springboot2/service/AnimeService.java
+++ src/main/java/silva/jhonatan/springboot2/service/AnimeService.java
@@ -6,14 +6,20 @@ import org.springframework.web.server.ResponseStatusException;
 import silva.jhonatan.springboot2.domain.Anime;
 import silva.jhonatan.springboot2.repository.AnimeRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Onde fazemos as regras de negócio
  */
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(new Anime(1L, "One punch man"), new Anime(2L, "One piece"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "One punch man"), new Anime(2L, "One piece")));
+    }
 
     /**
      * To do
@@ -32,6 +38,7 @@ public class AnimeService {
      * Além disso, ainda vem um stack tracer muito extenso que vc pode reduzir ou ignorar na resposta
      * indo em resources/application.yml e adicionar server: error: include-stacktrace: on_param
      * para aparecer o param precisa colocar no caminho http ?trace=true
+     *
      * @param id
      * @return
      */
@@ -40,5 +47,11 @@ public class AnimeService {
                 .filter(animes -> animes.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 10000000));
+        animes.add(anime);
+        return anime;
     }
 }

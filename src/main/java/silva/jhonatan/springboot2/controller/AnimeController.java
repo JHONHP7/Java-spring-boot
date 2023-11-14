f52aea18f9daafbc42a01037cb9abd64a2938f55
jsilva6 <jhonatan.silva@fazenda.rj.gov.br>
2023-11-14 11:34:27 -0300

    Aula 08 - Método GET parte 2

--- src/main/java/silva/jhonatan/springboot2/controller/AnimeController.java
+++ src/main/java/silva/jhonatan/springboot2/controller/AnimeController.java
@@ -3,7 +3,10 @@ package silva.jhonatan.springboot2.controller;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import silva.jhonatan.springboot2.domain.Anime;
@@ -33,9 +36,26 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        /**
+         * Posso retornar o status da requisição
+         * Usando return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+         * Ou usando
+         */
+        return ResponseEntity.ok(animeService.listAll());
     }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id) {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        /**
+         * Posso retornar o status da requisição
+         * Usando return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+         * Ou usando
+         */
+        return ResponseEntity.ok(animeService.findById(id));
+    }
+
 
 }
--- src/main/java/silva/jhonatan/springboot2/service/AnimeService.java
+++ src/main/java/silva/jhonatan/springboot2/service/AnimeService.java
@@ -1,6 +1,8 @@
 package silva.jhonatan.springboot2.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import silva.jhonatan.springboot2.domain.Anime;
 import silva.jhonatan.springboot2.repository.AnimeRepository;
 
@@ -11,12 +13,32 @@ import java.util.List;
  */
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(new Anime(1L, "One punch man"), new Anime(2L, "One piece"));
+
     /**
      * To do
      * private final AnimeRepository animeRepository
+     *
      * @return
      */
     public List<Anime> listAll() {
-        return List.of(new Anime(1L, "One punch man"), new Anime(2L, "One piece"));
+        return animes;
+    }
+
+    /**
+     * Buscar anime por id, e caso não retorne nada, usamos o bad request ao invés de 404 not found
+     * e a message tbm par quando existir o exception na requisição aparaça a msm que possa ser usada pelo
+     * front
+     * Além disso, ainda vem um stack tracer muito extenso que vc pode reduzir ou ignorar na resposta
+     * indo em resources/application.yml e adicionar server: error: include-stacktrace: on_param
+     * para aparecer o param precisa colocar no caminho http ?trace=true
+     * @param id
+     * @return
+     */
+    public Anime findById(Long id) {
+        return animes.stream()
+                .filter(animes -> animes.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
